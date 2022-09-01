44237c093db92838e7ab5cffbe0db1148298e140
Muzammil Saeed <muzammilsaeed49@gmail.com>
2022-09-02 01:58:59 +0500

    ok

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 // import ModalVideo from "react-modal-video";
-import "../modalVideo.scss";
+// import "../modalVideo.scss";
 
 import { BsPlayCircleFill } from "react-icons/bs";
 
